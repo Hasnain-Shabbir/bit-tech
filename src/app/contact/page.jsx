96cb399d3723408c608d46402384a8b745089b8d
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-07-19 00:16:53 +0500

    create contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { Button } from "../../components";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className={styles.contactWrapper}>
+          <h1 className={styles.title}>Let's Keep in Touch</h1>
+          <div className={styles.content}>
+            <div className={styles.imgContainer}>
+              <Image
+                src="/contact.png"
+                alt="contact"
+                fill={true}
+                className={styles.img}
+              />
+            </div>
+            <form className={styles.form}>
+              <input type="text" placeholder="name" className={styles.input} />
+              <input type="email" placeholder="name" className={styles.input} />
+              <textarea
+                name="message"
+                cols="30"
+                rows="10"
+                className={styles.textArea}
+              ></textarea>
+              <Button url="#" text="send" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;

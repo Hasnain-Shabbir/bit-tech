64523940894a26f969106e74bd96ed62c20703cd
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-07-18 18:54:21 +0500

    create hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import HeroImage from "/public/hero.png";
+import styles from "./page.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <header>
+      <div className="container">
+        <div className={styles.heroWrapper}>
+          <div className={styles.box}>
+            <h1 className={styles.title}>
+              Better design for your digital products.
+            </h1>
+            <p className={styles.desc}>
+              Turning your Idea into Reality. We bring together the teams from
+              the global tech industry.
+            </p>
+            <button className={styles.heroBtn}>See Our Works</button>
+          </div>
+          <div className={styles.box}>
+            <Image
+              src={HeroImage}
+              alt="hero-image"
+              className={styles.heroImg}
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Home;

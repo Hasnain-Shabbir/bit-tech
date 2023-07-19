9de5fc93a06f2072ac33978bad7b7bd867eb2db7
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-07-19 13:20:35 +0500

    craete portfolio category page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Image from "next/image";
+import { Button } from "../../../components";
+import { portfolioData } from "./portfolioData";
+import styles from "./category.module.css";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  const { category } = params;
+  const portfolioItems = portfolioData[category];
+
+  return (
+    <div>
+      <h1 className={styles.catTitle}>{category}</h1>
+      {portfolioItems.map((data) => (
+        <div className={styles.item} key={data.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{data.title}</h1>
+            <p className={styles.desc}>{data.desc}</p>
+            <Button url="#" text="See More" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              src={data.image}
+              fill={true}
+              alt="illustration"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
---
+++ src/app/portfolio/[category]/portfolioData.js
@@ -0,0 +1,71 @@
+export const portfolioData = {
+  illustrations: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/1418595/pexels-photo-1418595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      title: "Amazing One",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/1910225/pexels-photo-1910225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      title: "Great One",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/3631430/pexels-photo-3631430.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ],
+  websites: [
+    {
+      id: 1,
+      title: "Ninja Blog",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      title: "E-Shop Online Store",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/251225/pexels-photo-251225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      title: "Shareme Photo Sharing",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ],
+  applications: [
+    {
+      id: 1,
+      title: "Connecter",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/238480/pexels-photo-238480.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      title: "Done With It",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/5082578/pexels-photo-5082578.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      title: "Mafh",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Maxime laboriosam Perspiciatis nesciunt cumque harum iste eius quae ullamab porro consectetur sunt nostrum enim odit repellat debitis minussquaerat assumenda.",
+      image:
+        "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ],
+};

536c08f33c90dd93f95960d5ce75db92b554aa2e
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-07-19 15:34:26 +0500

    create blog post page

--- src/app/blog/[blogId]/page.jsx
+++ src/app/blog/[blogId]/page.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const BlogPost = () => {
-  return <div>BlogPost</div>;
+  const data = {
+    title: "Hello this is blog page",
+    desc: "here we are going to learn about the top programming languages in the world",
+  };
+  return (
+    <section>
+      <div className="container">
+        <div className={styles.container}>
+          <div className={styles.top}>
+            <div className={styles.info}>
+              <h1 className={styles.title}>{data.title}</h1>
+              <p className={styles.desc}>{data.desc}</p>
+              <div className={styles.author}>
+                <Image
+                  src={data.img}
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.avatar}
+                />
+                <span className={styles.username}>{data.username}</span>
+              </div>
+            </div>
+            <div className={styles.imageContainer}>
+              <Image
+                src={data.img}
+                alt=""
+                fill={true}
+                className={styles.image}
+              />
+            </div>
+          </div>
+          <div className={styles.content}>
+            <p className={styles.text}>{data.content}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BlogPost;

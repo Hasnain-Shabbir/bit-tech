c19c52efe213afb935fe1c2a8bd95b161cdfe9c5
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-07-20 18:55:08 +0500

    create dashboard page and posts api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,11 +3,31 @@ import Post from "../../../models/Post";
 import connect from "../../../utils/db";
 
 export const GET = async (request) => {
+  const url = new URL(request.url);
+
+  const username = url.searchParams.get("username");
+
   try {
     await connect();
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
 
     return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(error.message, { status: 400 });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    const body = await request.json();
+    await connect();
+
+    const newPost = new Post(body);
+
+    await newPost.save();
+
+    return new NextResponse("Post has been created", { status: 201 });
   } catch (error) {
     console.log(error);
     return new NextResponse(error.message, { status: 400 });
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,17 +2,92 @@
 import React from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import useSWR from "swr";
+import Image from "next/image";
+import styles from "./dashboard.module.css";
 
 const Dashboard = () => {
   const session = useSession();
   const router = useRouter();
-  console.log("🚀 ~ file: page.jsx:7 ~ Dashboard ~ session:", session);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user?.name}`,
+    fetcher,
+  );
 
   if (session.status === "loading") return <p>Loading...</p>;
 
   if (session.status === "unauthenticated") router.push("/dashboard/login");
 
-  if (session.status === "authenticated") return <div>Dashboard</div>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch("api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      e.target[0].value = "";
+      e.target[1].value = "";
+      e.target[2].value = "";
+      e.target[3].value = "";
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (session.status === "authenticated")
+    return (
+      <section>
+        <div className="container">
+          <div className={styles.wrapper}>
+            <div className={styles.posts}>
+              {isLoading
+                ? "loading..."
+                : data?.map((post) => (
+                    <div className={styles.post} key={post._id}>
+                      <div className={styles.imgContainer}>
+                        <Image src={post.img} alt={post.title} fill={true} />
+                      </div>
+                      <h2 className={styles.postTitle}>{post.title}</h2>
+                      <span className={styles.delete}>X</span>
+                    </div>
+                  ))}
+            </div>
+            <form className={styles.addPost} onSubmit={handleSubmit}>
+              <h1 className={styles.wrapper}>Add New Post</h1>
+              <input type="text" placeholder="Title" className={styles.input} />
+              <input
+                type="text"
+                placeholder="Description"
+                className={styles.input}
+              />
+              <input type="text" placeholder="Image" className={styles.input} />
+              <textarea
+                placeholder="Content"
+                cols="30"
+                rows="10"
+                className={styles.textArea}
+              ></textarea>
+              <button className={styles.button}>Create Post</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    );
 };
 
 export default Dashboard;

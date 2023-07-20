261494b36100fe14a864a54f8907893dd085e2ad
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-07-20 17:23:08 +0500

    add logout functionality

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
   const session = useSession();
+  console.log("🚀 ~ file: page.jsx:7 ~ Dashboard ~ session:", session);
 
   return <div>Dashboard</div>;
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
 import { navData } from "./navData";
 import { DarkModeToggle } from "../../components";
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
+  const session = useSession();
+
   return (
     <nav className={`container ${styles.navbar}`}>
       <Link href="/" className={styles.logo}>
@@ -19,7 +23,11 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <button className={styles.logout}>Logout</button>
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
     </nav>
   );

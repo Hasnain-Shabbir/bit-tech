0390cc91ed4371ddf1d02307dc111a78a24c0f3e
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-07-20 15:25:38 +0500

    add google login

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,7 +1,14 @@
+"use client";
 import React from "react";
+import { signIn } from "next-auth/react";
+import styles from "./login.module.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <button onClick={() => signIn("google")}>Login with Google</button>
+    </div>
+  );
 };
 
 export default Login;
